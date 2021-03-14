6d0b54932127538b93b11bb930fbc2f4b56f248b
null223 <sumidamao@gmail.com>
2021-03-14 21:34:46 +0900

    add profile style

--- pages/about.js
+++ pages/about.js
@@ -35,28 +35,28 @@ function About({ data }) {
       <p>ポートフォリオ</p>
     </PageHead>
     <main>
+      <StProfile bgImage={data.profile_bg.url}>
+        <div className="p-3">
+          <div className="profile-image">
+            <div><img src={data.icon.url} alt={data.name} /></div>
+          </div>
+          <div className="profile-body">
+            <h2>{data.name}</h2>
+            <p>{data.profile}</p>
+            <div className="profile-sns">
+              <Link href={"https://twitter.com/" + process.env.NEXT_PUBLIC_TWITTER_ID}>
+                <a
+                  target="_blank"
+                  rel="noopener"
+                ><i className="fab fa-twitter-square fa-2x" /></a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </StProfile>
       <div className="container">
         <StAbout>
-          <Box
-            header="プロフィール"
-          >
-            <div className="px-4">
-              <div
-                className="mb-3"
-                dangerouslySetInnerHTML={{
-                  __html: data.profile
-                }}
-              />
-              <p className="mb-0">
-                <Link href={"https://twitter.com/" + data.twitterid}>
-                  <a
-                    target="_blank"
-                    rel="noopener"
-                  ><i className="fab fa-twitter-square fa-2x" /></a>
-                </Link>
-              </p>
-            </div>
-          </Box>
+          
           <Box
             header="仕事について"
           >
@@ -91,6 +91,52 @@ const StAbout = styled.section`
 ${({theme}) => css`
   & .fa-twitter-square {
     color: #1DA1F2 !important;
+  }
+`}`
+
+const StProfile = styled.section`
+${({theme, bgImage}) => css`
+  position: relative;
+  width: 100%;
+  margin-top: -3rem;
+  margin-bottom: 3rem;
+  background-image: url(${bgImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  &::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-color: ${theme.colors.black};
+    opacity: .7;
+  }
+  & > div {
+    position: relative;
+  }
+  .profile-image {
+    margin: 30px auto 0;
+    display: block;
+    max-width: 250px;
+    & > div {
+      border-radius: 50%;
+      overflow: hidden;
+      border: 5px solid ${theme.colors.white};
+    }
+    @media (max-width: 768px) {
+      max-width: 200px;
+    }
+  }
+  .profile-body {
+    max-width: 600px;
+    width: 100%;
+    margin: 32px auto 50px;
+    text-align: center;
+    & h2, & p {
+      color: ${theme.colors.white};
+    }
   }
 `}`
 
