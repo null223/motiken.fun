d840922d2ee8f61ec323a1bb8526f13988fea1f0
null223 <sumidamao@gmail.com>
2021-03-14 20:44:21 +0900

    fix blog detail

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,31 +1,8 @@
 import { htmlToText } from 'html-to-text'
+import styled, { css } from 'styled-components'
 
 import Head from '@/components/Head'
-
-export default function BlogId({ blog }) {
-  return (<>
-    <Head>
-      <title>motiken.fun マイページ</title>
-      <meta property="og:title" content={blog.title+" motiken.fun"} />
-      <meta property="og:description" content={htmlToText(blog.body, {limits: 40})} />
-      <meta property="og:image" content={blog.image.url} />
-      <meta property="og:type" content="article" />
-    </Head>
-    <main>
-      <div className="container">
-        <img src={blog.image.url} alt={blog.title} />
-        <h1>{blog.title}</h1>
-        <p>{blog.publishedAt}</p>
-        <p className="category">{blog.category && `${blog.category.name}`}</p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `${blog.body}`,
-          }}
-        />
-      </div>
-    </main>
-  </>);
-}
+import Box from '@/components/Box'
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
@@ -57,3 +34,45 @@ export const getStaticProps = async context => {
     },
   };
 };
+
+export default function BlogId({ blog }) {
+  return (<>
+    <Head>
+      <title>{blog.title+" motiken.fun"}</title>
+      <meta property="og:title" content={blog.title+" motiken.fun"} />
+      <meta property="og:description" content={htmlToText(blog.body, {limits: 40})} />
+      <meta property="og:image" content={blog.image.url} />
+      <meta property="og:type" content="article" />
+    </Head>
+    <main>
+      <div className="container mt-4">
+        <Box>
+          <StBlogId className="px-3">
+            <div className="blog-image">
+              <img src={blog.image.url} alt={blog.title} />
+            </div>
+            <h1>{blog.title}</h1>
+            <p>{blog.publishedAt}</p>
+            <p className="category">{blog.category && `${blog.category.name}`}</p>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `${blog.body}`,
+              }}
+            />
+          </StBlogId>
+        </Box>
+      </div>
+    </main>
+  </>);
+}
+
+const StBlogId = styled.div`
+${({theme}) => css`
+  .blog-image {
+    margin-top: 1.5rem;
+  }
+  & h1 {
+    margin-top: 1rem;
+    font-size: 1.25rem;
+  }
+`}`
