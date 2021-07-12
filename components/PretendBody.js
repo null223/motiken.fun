c4fdecb9e2f37cd4329e5176d77fefe7588d13d2
null223 <sumida0223@gmail.com>
2021-07-12 21:05:24 +0900

    contactページは後でやる

--- components/PretendBody.js
+++ components/PretendBody.js
@@ -2,7 +2,7 @@
 function PretendBody() {
   return (
     <>
-      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
+      {/*<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>*/}
     </>
   )
 }
